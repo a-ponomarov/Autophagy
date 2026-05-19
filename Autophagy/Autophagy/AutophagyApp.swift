928921b5acc461148aeddb88e8a34a314d304d73
Andrew Ponomarov <andrii.ponomarov.apps@gmail.com>
2026-05-19 13:43:17 +0300

    Create Xcode project

---
+++ Autophagy/Autophagy/AutophagyApp.swift
@@ -0,0 +1,19 @@
+//
+//  AutophagyApp.swift
+//  Autophagy
+//
+//  Created by Andrew Ponomarov on 19/5/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AutophagyApp: App {
+  
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
